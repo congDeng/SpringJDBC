e18af08e94c6c034f37314b1676ccf12e6db2f3f
congcong <d948154418cc@gmail.com>
2024-08-19 22:43:05 +0800

    create model/service/repo layer

---
+++ src/main/java/com/example/SpringJDBC/SpringJdbcApplication.java
@@ -0,0 +1,28 @@
+package com.example.SpringJDBC;
+
+import com.example.SpringJDBC.model.Student;
+import com.example.SpringJDBC.service.StudentService;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+@SpringBootApplication
+public class SpringJdbcApplication {
+
+	public static void main(String[] args) {
+		ConfigurableApplicationContext applicationContext = SpringApplication.run(SpringJdbcApplication.class, args);
+
+		Student student = applicationContext.getBean(Student.class);
+		student.setRollNo(101);
+		student.setName("Curry");
+		student.setMarks(99);
+
+		StudentService studentService = applicationContext.getBean(StudentService.class);
+		studentService.addStudent(student);
+
+		System.out.println(studentService.getAllStudents());
+
+
+	}
+
+}
---
+++ src/main/java/com/example/SpringJDBC/repo/StudentRepo.java
@@ -0,0 +1,19 @@
+package com.example.SpringJDBC.repo;
+
+import com.example.SpringJDBC.model.Student;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Repository
+public class StudentRepo {
+    public void save(Student student) {
+        System.out.println("student saved");
+    }
+
+    public List<Student> findAll() {
+        List<Student> students = new ArrayList<>();
+        return students;
+    }
+}
